20b7db980fd683c0ccc985017be6bb82b981f216
Taosif Bin Murtaza <dev.taosif.net@gmail.com>
2025-04-21 23:48:01 +0600

    Villa list Add

---
+++ ResortManagement/Controllers/VillaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResortManagement.Infrastructure.Data;
+
+namespace ResortManagement.Controllers;
+
+public class VillaController : Controller
+{
+    private readonly ApplicationDbContext _context;
+    public VillaController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public IActionResult Index()
+    {
+        var data = _context.Villas.ToList();
+
+        return View(data);
+    }
+}
