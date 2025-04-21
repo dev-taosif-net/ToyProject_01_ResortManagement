e45fda294f11ff2c7569fba45a9eddbc01cd1da7
Taosif Bin Murtaza <dev.taosif.net@gmail.com>
2025-04-21 22:37:42 +0600

    Seeding

--- ResortManagement.Infrastructure/Data/ApplicationDbContext.cs
+++ ResortManagement.Infrastructure/Data/ApplicationDbContext.cs
@@ -16,5 +16,93 @@ public class ApplicationDbContext : DbContext
 
     public DbSet<Villa> Villas { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        ////base.OnModelCreating(modelBuilder);
+        ////modelBuilder.Entity<Villa>().ToTable("Villas");
+        ////modelBuilder.Entity<Villa>().HasKey(v => v.Id);
+        ////modelBuilder.Entity<Villa>().Property(v => v.Name).IsRequired().HasMaxLength(100);
+        ////modelBuilder.Entity<Villa>().Property(v => v.Description).HasMaxLength(500);
+        ////modelBuilder.Entity<Villa>().Property(v => v.Price).IsRequired();
+        ////modelBuilder.Entity<Villa>().Property(v => v.Sqft).IsRequired();
+        ////modelBuilder.Entity<Villa>().Property(v => v.Occupancy).IsRequired();
+        ////modelBuilder.Entity<Villa>().Property(v => v.ImageUrl).HasMaxLength(200);
+        ////modelBuilder.Entity<Villa>().Property(v => v.VillaNumber).IsRequired();
+        ///
+
+        modelBuilder.Entity<Villa>().HasData(new Villa
+        {
+            Id = 1,
+            Name = "Royal Villa",
+            Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+            ImageUrl = "https://placehold.co/600x400",
+            Occupancy = 4,
+            Price = 200,
+            Sqft = 550,
+            CreatedDate = new DateTime(2025, 04, 21),
+            UpdatedDate = new DateTime(2025, 04, 21)
+        },
+        new Villa
+        {
+            Id = 2,
+            Name = "Premium Pool Villa",
+            Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+            ImageUrl = "https://placehold.co/600x400",
+            Occupancy = 4,
+            Price = 300,
+            Sqft = 600,
+            CreatedDate = new DateTime(2025, 04, 21),
+            UpdatedDate = new DateTime(2025, 04, 21)
+        },
+        new Villa
+        {
+            Id = 3,
+            Name = "Luxury Pool Villa",
+            Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+            ImageUrl = "https://placehold.co/600x400",
+            Occupancy = 4,
+            Price = 400,
+            Sqft = 700,
+            CreatedDate = new DateTime(2025, 04, 21),
+            UpdatedDate = new DateTime(2025, 04, 21)
+        },
+        new Villa
+        {
+            Id = 4,
+            Name = "Medium Pool Villa",
+            Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+            ImageUrl = "https://placehold.co/600x400",
+            Occupancy = 4,
+            Price = 500,
+            Sqft = 800,
+            CreatedDate = new DateTime(2025, 04, 21),
+            UpdatedDate = new DateTime(2025, 04, 21)
+        },
+        new Villa
+        {
+            Id = 5,
+            Name = "South face Pool Villa",
+            Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+            ImageUrl = "https://placehold.co/600x400",
+            Occupancy = 4,
+            Price = 600,
+            Sqft = 900,
+            CreatedDate = new DateTime(2025, 04, 21),
+            UpdatedDate = new DateTime(2025, 04, 21)
+        },
+        new Villa
+        {
+            Id = 6,
+            Name = "Simantw Villa",
+            Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+            ImageUrl = "https://placehold.co/600x400",
+            Occupancy = 4,
+            Price = 700,
+            Sqft = 1000,
+            CreatedDate = new DateTime(2025, 04, 21),
+            UpdatedDate = new DateTime(2025, 04, 21)
+        });
+    }
+
 }
 
---
+++ ResortManagement.Infrastructure/Migrations/20250421163700_seedVilla.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ResortManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedVilla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "CreatedDate", "Description", "ImageUrl", "IsReserved", "Name", "Occupancy", "Price", "Sqft", "UpdatedDate", "VillaNumber" },
+                values: new object[,]
+                {
+                    { 1L, new DateTime(2025, 4, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x400", false, "Royal Villa", 4, 200.0, 550, new DateTime(2025, 4, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), 0 },
+                    { 2L, new DateTime(2025, 4, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x400", false, "Premium Pool Villa", 4, 300.0, 600, new DateTime(2025, 4, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), 0 },
+                    { 3L, new DateTime(2025, 4, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x400", false, "Luxury Pool Villa", 4, 400.0, 700, new DateTime(2025, 4, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), 0 },
+                    { 4L, new DateTime(2025, 4, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x400", false, "Medium Pool Villa", 4, 500.0, 800, new DateTime(2025, 4, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), 0 },
+                    { 5L, new DateTime(2025, 4, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x400", false, "South face Pool Villa", 4, 600.0, 900, new DateTime(2025, 4, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), 0 },
+                    { 6L, new DateTime(2025, 4, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x400", false, "Simantw Villa", 4, 700.0, 1000, new DateTime(2025, 4, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), 0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5L);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 6L);
+        }
+    }
+}
